aa9a7277007766094d89cd9714c376fcb12c0362
Pascal Rainville <pascal.rainville@gmail.com>
2014-06-25 14:59:22 -0400

    .

---
+++ lib.c
@@ -0,0 +1,15 @@
+int add(int x, int y) {
+  return x + y;
+}
+
+int sus(int x, int y) {
+  return x - y;
+}
+
+int mult(int x, int y) {
+  return x * y;
+}
+
+int div(int x, int y) {
+  return x / y;
+}
