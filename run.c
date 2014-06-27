0a53732d53628ca2f24f6e63ffefaff1b8aed191
Pascal Rainville <pascal.rainville@gmail.com>
2014-06-27 15:13:58 -0400

    .

--- run.c
+++ run.c
@@ -1,5 +1,11 @@
+#include <string.h>
+
 void main(int argc, char* argv[]) {
-  system("gcc -o build/icsh lib.c src/bind/bind_lib.c src/bind/glue.c main.c -lncurses");
+  // TODO: list src/bind and add all the source files there.
+  char cmd[256] = "gcc -o build/icsh ";
+  strcat(cmd, "lib.c src/bind/bind_lib.c src/bind/glue.c main.c");
+  strcat(cmd, " -lncurses");
+  system(cmd);
   system("./build/icsh");
 //gcc -g -o build/debug lib.c src/bind/bind.c src/bind/glue.c main.c -lncurses && gdb build/debug
 }
