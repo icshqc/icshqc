12533f2f907c5a4b575ae361cc48ef9ce212044f
Pascal Rainville <pascal.rainville@gmail.com>
2014-06-29 16:25:12 -0400

    .

--- run.c
+++ run.c
@@ -12,9 +12,11 @@ void main(int argc, char* argv[]) {
   }
 
   // TODO: list src/bind and add all the source files there.
-  char cmd[256] = "gcc -o build/icsh ";
+  char cmd[256] = "gcc -o ";
   if (debug_mode) {
-    strcat(cmd, "-g ");
+    strcat(cmd, "build/debug -g ");
+  } else {
+    strcat(cmd, "build/icsh ");
   }
   strcat(cmd, "lib.c src/bind/bind_lib.c src/bind/glue.c main.c");
   strcat(cmd, " -lncurses");
